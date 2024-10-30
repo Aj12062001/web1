4a29f35323a1af8108963d4f906216612682c75a
Ajin Jose <105265228+Aj12062001@users.noreply.github.com>
2024-10-30 10:14:24 +0530

    Add files via upload

---
+++ html 3/q1sub.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("colorButton");
+
+button.addEventListener('click', function() {
+    document.body.style.backgroundColor = "red";
+});
+
+button.addEventListener('dblclick', function() {
+    document.body.style.backgroundColor = "green";
+});
